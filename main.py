103f08cbe6590396fbb9a73fcc6a704b8550dffd
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-02-11 07:39:57 +0000

    a

--- main.py
+++ main.py
@@ -9,18 +9,38 @@ class Database:
     def fetch_employees(self):
         with sqlite3.connect(self.__db_name()) as con:
             con.row_factory = sqlite3.Row
-            with closing(con.cursor()) as cursor:
-                cursor.execute('SELECT employee_id, firstname, lastname FROM employees')
-                return [dict(row) for row in cursor.fetchall()]
+            with closing(con.cursor()) as cur:
+                cur.execute('SELECT employee_id, firstname, lastname FROM employees')
+                return [dict(row) for row in cur.fetchall()]
+
+    def add_employees(self, data: list[tuple['str','str']]):
+        with sqlite3.connect(self.__db_name()) as con:
+            with closing(con.cursor()) as cur:
+                cur.executemany("INSERT INTO employees(firstname, lastname) VALUES(?, ?)", data)
+                
+    def add_employees_dict(self, data: list[dict['str','str']]):
+        with sqlite3.connect(self.__db_name()) as con:
+            with closing(con.cursor()) as cur:
+                cur.executemany("INSERT INTO employees(firstname, lastname) VALUES(:firstname, :lastname)", data)
 
 
 def main():
     db = Database()
-    employees = db.fetch_employees()
-    print('list of employees'.capitalize())
-    print('--------------------------------')
-    for emp in sorted(employees, key=lambda x: x['firstname']):
-        print(f'{emp.get('firstname')} {emp.get('lastname')}'.title())
+    # db.add_employees([
+    #     ('John','Smith'),
+    #     ('Joe','Smith')
+    # ])
+
+    db.add_employees_dict([
+        {'firstname': 'Barry', 'lastname': 'Smith'},
+        {'firstname': 'Amy', 'lastname': 'Larson'},
+    ])
+    print(db.fetch_employees())
+    # employees = db.fetch_employees()
+    # print('list of employees'.capitalize())
+    # print('--------------------------------')
+    # for emp in sorted(employees, key=lambda x: x['firstname']):
+    #     print(f'{emp.get('firstname')} {emp.get('lastname')}'.title())
 
 
 if __name__ == '__main__':
